95404da13f69ff7610e33e5185721d47e7859720
Zoran Regvart <zregvart@apache.org>
2020-04-16 15:10:33 +0200

    feat(build): image optimization
    This adds image optimization using imageoptim. According to the output
    this saves 5.49 MB or 19%. The tradeoff is that the image optimization
    takes a long time to perform and would increase the build times, on my
    machine ~3:30min more.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
 const env = process.env.CAMEL_ENV || 'development'
 
 gulp.task('minify', (done) => {
@@ -16,5 +17,26 @@ gulp.task('minify', (done) => {
       conservativeCollapse: true,
       useShortDoctype: true
     }))
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('imagemin', (done) => {
+  if (env !== 'production') {
+    done();
+    return;
+  }
+
+  return gulp.src('public/**/*.{svg,png,gif,jpg,jpeg}')
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({ plugins: [
+        { removeViewBox: false },
+        { cleanupIDs: { remove: false } },
+        { removeTitle: false },
+        { removeDesc: false }
+      ] })
+    ]))
     .pipe(gulp.dest('public'));
 });
